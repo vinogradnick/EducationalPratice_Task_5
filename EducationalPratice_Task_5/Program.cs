1e9df8b0e2444f45e139d07df439d606eade5fe1
vinogradovnick32 <vinogradovnick32@gmail.com>
2018-05-18 15:18:37 +0500

    first commit

--- EducationalPratice_Task_5/Program.cs
+++ EducationalPratice_Task_5/Program.cs
@@ -6,10 +6,54 @@ using System.Threading.Tasks;
 
 namespace EducationalPratice_Task_5
 {
+    /// <summary>
+    /// Дана действительная квадратичная матрица порядка 9
+    /// Получить целочисленную квадратную матрицу того же порядка,
+    ///  в которой элемент равен еденице, 
+    ///     если соответствующий ему элемент исходной матрицы больше элемента, 
+    ///         расположенного в его строке на главной диагонали,
+    ///  и равен нулю в противном случае.
+    /// </summary>
     class Program
     {
+        static Random random = new Random();
         static void Main(string[] args)
         {
+            Console.WriteLine("Получить целочисленную квадратную матрицу того же порядка, в которой элемент равен еденице,\n если соответствующий ему элемент исходной матрицы больше элемента, расположенного в его строке на главной диагонали,\n и равен нулю в противном случае.");
+            double[,] matrix = new double[9, 9];
+            double diag = 0;//Элемент исходной матрицы по диагонали
+
+            Console.WriteLine("Исходная матрица:");
+            //Заполнение матрицы
+            for (int i = 0; i < matrix.GetLength(0); i++)
+                for (int j = 0; j < matrix.GetLength(1); j++)
+                    matrix[i, j] = Math.Round(random.NextDouble(), 3);
+            //Печать матрицы
+            for (int i = 0; i < matrix.GetLength(0); i++)
+            {
+                for (int j = 0; j < matrix.GetLength(1); j++)
+                    Console.Write($" {matrix[i, j],5} ");
+                Console.WriteLine();
+            }
+            //Выполнение действия над матрицей
+            for (int i = 0; i < matrix.GetLength(0); i++)
+            {
+                diag = matrix[i, i];//Получение элемента матрицы
+                for (int j = 0; j < matrix.GetLength(1); j++)
+                    if (matrix[i, j] < diag)
+                        matrix[i, j] = 1;
+                    else
+                        matrix[i, j] = 0;
+            }
+            Console.WriteLine("Итоговая матрица:");
+            for (int i = 0; i < matrix.GetLength(0); i++)
+            {
+                for (int j = 0; j < matrix.GetLength(1); j++)
+                    Console.Write($" {matrix[i, j],5} ");
+                Console.WriteLine();
+            }
+
+            Console.Read();
         }
     }
 }
